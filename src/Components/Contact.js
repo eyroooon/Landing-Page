d970acda99cdbbfa6ef4a4cbae04cd069fe4395a
Eyrooooon <aaron.github@carelulu.com>
2023-08-27 07:40:09 +0800

    add meta tags

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import axios from 'axios';
-
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
 const Contact = () => {
   const [phone, setPhone] = React.useState('');
 
   const handleSubmit = async () => {
-    const data = {
-      data: [
-        {
-          Last_Name: 'Unknown',
-          Year: 'NA',
-          Model: 'NA',
-          Part: 'NA',
-          Make: 'NA',
-          Problem: 'NA',
-          Case: 'SAAS',
-          Phone_3: phone,
-          Call_Status: 'NA',
-          Intent: 'NA',
-          Number_Type: 'NA',
-          Carrier: 'NA',
-        },
-      ],
-    };
-
     try {
-      const response = await axios.post('https://master--meek-custard-97937a.netlify.app/.netlify/functions/myFunction', data);
+      const response = await axios.post('https://master--meek-custard-97937a.netlify.app/.netlify/functions/myFunction', phone);
 
       if (response.status === 200) {
         console.log('Data saved to Zoho CRM', response.data);
+        window.location.reload();
       } else {
         console.error('Error saving data to Zoho CRM', response.data);
       }
@@ -42,7 +25,7 @@ const Contact = () => {
       <h1 className="primary-heading">Want a demo?</h1>
       <h1 className="primary-heading">Let Us Help You</h1>
       <div className="contact-form-container">
-        <input className="outlined-input" type="text" placeholder="Enter Your Mobile Phone No." value={phone} onChange={(e) => setPhone(e.target.value)} />
+        <PhoneInput className="outlined-input" placeholder="Enter your phone number" value={phone} onChange={setPhone} />
         <button className="secondary-button" onClick={handleSubmit}>
           Submit
         </button>
