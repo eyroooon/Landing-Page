6ff841b4f5b8798e3a61b7896f1504f9b6874fb4
Eyrooooon <aaron.github@carelulu.com>
2023-08-31 21:28:36 +0800

    fix mobile view

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,14 +8,14 @@ const Home = () => {
     <div className="home-container">
       <Navbar />
       <div className="home-banner-container">
-        <div className="home-bannerImage-container">
-          <img src={BannerBackground} alt="" loading="lazy"/>
-        </div>
-        <div className="home-text-section">
+        <div className="work-section-top">
+          <div className="home-bannerImage-container">
+            <img src={BannerBackground} alt="" loading="lazy" />
+          </div>
           <h1 className="primary-heading">The Best Partner for Your Yard's Calls!</h1>
           <p className="primary-text">Our 24/7 AI answers all calls & breaks sales records. Heard our example calls?</p>
           <button className="secondary-button">
-            See How It Works <FiArrowRight />{' '}
+            See How It Works <FiArrowRight />
           </button>
         </div>
         <div className="home-image-section">
@@ -24,7 +24,7 @@ const Home = () => {
               title="Video"
               src="https://www.facebook.com/plugins/video.php?height=323&href=https%3A%2F%2Fwww.facebook.com%2F100000237951579%2Fvideos%2F827553928777401%2F&show_text=false&width=560&t=0"
               allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-              loading="lazy" 
+              loading="lazy"
             ></iframe>
           </div>
         </div>
